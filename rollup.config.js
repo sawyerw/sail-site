fca7628ca6de71a636f966fa2c4641ca7a91e4c0
sawyerw <sawyerw221@gmail.com>
2026-04-20 10:19:39 -0400

    test vercel

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@ import babel from '@rollup/plugin-babel';
 import { rollupPluginHTML as html } from '@web/rollup-plugin-html';
 import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
 import esbuild from 'rollup-plugin-esbuild';
+import pkg from 'rollup-plugin-copy';
+const copy = pkg;
 
 export default {
   input: 'index.html',
@@ -20,6 +22,14 @@ export default {
     html({
       minify: true,
     }),
+    copy({
+      targets: [
+        {
+          src: 'assets/',
+          dest: `public/`,
+          flatten: false
+        },
+      ]}),
     /** Resolve bare module imports */
     nodeResolve(),
     /** Minify JS, compile JS to a lower language target */
